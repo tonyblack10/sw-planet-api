736e9880716f7aefcee960845e78fdd6a4f1a61e
Tony Costa <tonyblack10@gmail.com>
2023-01-22 01:24:22 -0300

    mockito nos testes de unidade

--- src/test/java/io/github/tonyblack10/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/io/github/tonyblack10/swplanetapi/domain/PlanetServiceTest.java
@@ -4,15 +4,27 @@ import static io.github.tonyblack10.swplanetapi.common.PlanetConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = PlanetService.class)
+// @SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
+    @InjectMocks
     private PlanetService planetService;
+
+    @Mock
+    private PlanetRepository planetRepository;
 
     @Test
     public void createPlanet_WithValidData_ReturnsPLanet() {
+        Mockito.when(planetRepository.save(PLANET))
+                .thenReturn(PLANET);
+
         var sut = planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
