3169160776e7daf654f316bad532c5ec79c280b2
Tony Costa <tonyblack10@gmail.com>
2023-01-22 21:50:08 -0300

    testando o cenario de sucesso no repository

---
+++ src/test/java/io/github/tonyblack10/swplanetapi/domain/PlanetRepositoryTest.java
@@ -0,0 +1,32 @@
+package io.github.tonyblack10.swplanetapi.domain;
+
+import static io.github.tonyblack10.swplanetapi.common.PlanetConstants.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest(classes = { PlanetRepository.class })
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPLanet() {
+        var planet = planetRepository.save(PLANET);
+
+        var sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
+}
