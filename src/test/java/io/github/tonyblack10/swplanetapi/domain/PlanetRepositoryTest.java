8e602f6c7e3144a7d300dc416ef5710a91b7330a
Tony Costa <tonyblack10@gmail.com>
2023-01-22 22:27:36 -0300

    testando o cenario de sucesso no controller

--- src/test/java/io/github/tonyblack10/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/io/github/tonyblack10/swplanetapi/domain/PlanetRepositoryTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest(classes = { PlanetRepository.class })
 @DataJpaTest
---
+++ src/test/java/io/github/tonyblack10/swplanetapi/web/PlanetControllerTest.java
@@ -0,0 +1,44 @@
+package io.github.tonyblack10.swplanetapi.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static io.github.tonyblack10.swplanetapi.common.PlanetConstants.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.tonyblack10.swplanetapi.domain.PlanetService;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsCreated() throws Exception {
+        var planetJson = objectMapper.writeValueAsString(PLANET);
+
+        Mockito.when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planets").content(planetJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+
+}
